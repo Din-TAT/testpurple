954c3def8bc6f030c89fc606816d26068cac1c0e
Динар <mahmutsindinar@gmail.com>
2026-02-09 23:23:11 -0500

    Упражнения по переменным Bash

---
+++ Переменные/Персональный_профиль_пользователя.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+read -p "Сколько лет?" age
+read -p "Из какого города?" city
+read -p "Какое хобби?" hobby 
+
+echo "| Имя: $1"
+echo "│ Возраст: $age лет"
+echo "| Город: $city"
+echo "| Хобби: $hobby"
+
